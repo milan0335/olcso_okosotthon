abebb11faa0f716d3dedfb9132b1d5172440b59f
root <root@core-ssh.local.hass.io>
2022-11-08 15:19:46 +0100

    Test of new 'commit code'

--- pushupdates.sh
+++ pushupdates.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+cd /home/homeassistant/.homeassistant
+source /srv/homeassistant/bin/activate
+hass --script check_config
+
 git add .
-git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
-git push -u origin master
+git status
+
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+
+git commit -m "${CHANGE_MSG}"
+git push origin master
+exit
